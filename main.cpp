dd8190512e0b24974f1dc0d449865b4ae66dcdfe
Unreallx <light.agma.io@gmail.com>
2026-05-24 16:36:07 +0300

    refactor: delete previous logger

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
         &app,
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection
-    );
+        );
     engine.loadFromModule("Elm327-Emulator", "Main");
 
     return app.exec();
