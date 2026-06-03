b3eab7e8add58a1d27c3f425de173c00d550a026
Unreallx <light.agma.io@gmail.com>
2026-06-03 21:52:45 +0300

    add new settings and parameters

--- src/core/state/ElmConfig.h
+++ src/core/state/ElmConfig.h
@@ -1,7 +1,20 @@
 #pragma once
 
+#include <QString>
+
 struct ElmConfig {
-    bool echoEnabled = true; // Отвечает за эхо
-    bool appendPrompt = true; // добавлять ли знак > в конце ответа
-    bool spacesEnabled = true; // отвечает за пробелы в hex ответах
+    /// x - число, либо 1 либо 0.
+
+    /// Базовые настройки отображения
+    bool echoEnabled = false;       // AT Ex - AT Ex (Повторять ли команду пользователя)
+    bool appendPrompt = true;      // Добавлять ли > в конце
+    bool spacesEnabled = true;     // AT Sx - AT Sx (Пробелы в ответах)
+    bool linefeedsEnabled = false;  // AT Lx / AT Lx (Добавлять ли \n после каждого \r)
+
+    /// Настройка протоколов и шины
+    int currentProtocol = 0;       // AT SP x (0 - автопоиск; выбор протокола)
+    bool headersEnabled = false;   // AT Hx / AT Hx (Показывать ли адреса блоков.) / В будущем поддержка мульти эбу
+
+    /// физические показатели
+    double voltage = 14.5;  // AT RV
 };
